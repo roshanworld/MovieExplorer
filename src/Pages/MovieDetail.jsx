36b96e1edda28cfe1963fc4fc9617d89efb5f426
roshan-barnwal <innovatewithroshan@gmail.com>
2026-08-01 18:11:59 +0530

    fix movie detail problem

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -8,27 +8,30 @@ function MovieDetail() {
     useEffect(() => {
         async function getMovie() {
             const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
-    
-          const data = await res.json();  
-          console.log(data);
-          SetMovie(data)
-        //   console.log(data)
+            //   const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+            const res = await fetch(
+                `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`
+            );
+
+            const data = await res.json();
+            console.log(data);
+            SetMovie(data)
+            //   console.log(data)
         }
         getMovie();
     }, [id])
 
-    if(!movie) return <p>Loading...</p>
+    if (!movie) return <p>Loading...</p>
 
-  return (
-    <div className="movie-detail">
-		<h2>{movie.Title}</h2>
-		<img alt={movie.Title} src={movie.Poster} />
-		<p><strong>Genre:</strong> {movie.Genre}</p>
-		<p><strong>Released:</strong> {movie.Released}</p>
-		<p><strong>Plot:</strong> {movie.Plot}</p>
-	</div>
-  )
+    return (
+        <div className="movie-detail">
+            <h2>{movie.Title}</h2>
+            <img alt={movie.Title} src={movie.Poster} />
+            <p><strong>Genre:</strong> {movie.Genre}</p>
+            <p><strong>Released:</strong> {movie.Released}</p>
+            <p><strong>Plot:</strong> {movie.Plot}</p>
+        </div>
+    )
 }
 
 export default MovieDetail
